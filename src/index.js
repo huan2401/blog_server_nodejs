01f0bb45766267d6fde6e5822efc44c5690ae9e6
DESKTOP-EP5LOO7\nguye <huannv.bhsoft@gmail.com>
2022-01-02 21:07:50 +0700

    pug sass

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const path = require('path');
-const port = 3000
+const port = 8080;
+
+//setup static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Require router
+const router = require(path.join(__dirname, 'resource/routers/routers'));
+// Dùng userRoute cho tất cả các route bắt đầu bằng </users<
+app.use('/', router);
 
 //setup template engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-//setup static file
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  // res.send('Hello World!');
-  res.render('index');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
